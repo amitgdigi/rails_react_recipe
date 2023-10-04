6aa557f18e3a7350f235771ccf0a16ff83fee8ed
amit gupta <amitguptaearn@gmail.com>
2023-10-04 19:21:38 +0530

    Completed crud of Recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_100317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "ingredients"
+    t.text "instruction"
+    t.string "image", default: "https://raw.githubusercontent.com/do-community/react_rails_recipe/master/app/assets/images/Sammy_Meal.jpg"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
